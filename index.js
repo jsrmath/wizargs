2c5d184212d4996c9544805222eda0e81b613790
Julian Rosenblum <jsrmath@gmail.com>
2015-07-14 01:00:47 -0400

    Added support for parsed arguments

--- index.js
+++ index.js
@@ -1,38 +1,49 @@
 var _ = require('underscore');
 
-// Take arguments and actual values and return object
-
 var _default = function (name, val) {
-	return {
-		default: true,
-		name: name,
-		val: val
-	}
+  return {
+    default: true,
+    name: name,
+    val: val
+  };
 };
 
+var parse = function (name, func) {
+  return {
+    parse: true,
+    name: name,
+    func: func
+  };
+};
+
+// Take arguments and actual values and return object
 var makeArgs = function (args, vals) {
-	var argObj = {};
+  var argObj = {};
 
-	_.each(args, function (arg, i) {
-		if (typeof(arg) === 'string') { // Regular argument, e.g. 'a'
-			argObj[arg] = vals[i];
-		}
-		else if (arg.default) { // Argument that has a default, e.g. wiz.default('a', 1)
-			argObj[arg.name] = vals[i] || arg.val;
-		}
-	});
+  _.each(args, function (arg, i) {
+    if (typeof(arg) === 'string') { // Regular argument, e.g. 'a'
+      argObj[arg] = vals[i];
+    }
+    else if (arg.default) { // Argument that has a default, e.g. wiz.default('a', 1)
+      argObj[arg.name] = vals[i] || arg.val;
+    }
+    else if (arg.parse) { // Argument that should be parsed, e.g. wiz.parse('a', parseInt)
+      argObj[arg.name] = arg.func(vals[i]);
+    }
+  });
 
-	return argObj;
+  return argObj;
 };
 
 module.exports.func = function (/* arguments */) {
-	var func = _.last(arguments);
-	var args = _.initial(arguments);
+  var func = _.last(arguments);
+  var args = _.initial(arguments);
 
-	return function () { // Return function that takes arbitrary number of arguments
-		var argObj = makeArgs(args, arguments)
-		func(argObj); // Call function with argument object
-	};
+  return function () { // Return function that takes arbitrary number of arguments
+    var argObj = makeArgs(args, arguments)
+    func(argObj); // Call function with argument object
+  };
 };
 
 module.exports.default = _default;
+module.exports.parse = parse;
--- test.js
+++ test.js
@@ -3,23 +3,31 @@ var assert = require('assert');
 
 describe('#func', function () {
   it('should create a function with regular arguments', function () {
-  	var foo = wiz.func('a', 'b', 'c', function (args) {
-  	  assert.equal(args.a, 0);
-  	  assert.equal(args.b, 1);
-  	  assert.equal(args.c, 2);
-  	});
-  	foo(0, 1, 2);
+    var foo = wiz.func('a', 'b', 'c', function (args) {
+      assert.equal(args.a, 0);
+      assert.equal(args.b, 1);
+      assert.equal(args.c, 2);
+    });
+    foo(0, 1, 2);
   });
 
   it('should create a function with default arguments', function () {
-  	var foo = wiz.func(wiz.default('a', 5), function (args) {
-  	  assert.equal(args.a, 5);
-  	});
-  	foo();
+    var foo = wiz.func(wiz.default('a', 5), function (args) {
+      assert.equal(args.a, 5);
+    });
+    foo();
 
-  	var bar = wiz.func(wiz.default('a', 5), function (args) {
-  	  assert.equal(args.a, 3);
-  	});
-  	bar(3);
+    var bar = wiz.func(wiz.default('a', 5), function (args) {
+      assert.equal(args.a, 3);
+    });
+    bar(3);
+  });
+
+  it('should create a function with parsed arguments', function () {
+    var foo = wiz.func(wiz.parse('a', parseInt), function (args) {
+      assert(args.a === 5);
+      assert(args.a !== '5');
+    });
+    foo('5');
   });
 });
