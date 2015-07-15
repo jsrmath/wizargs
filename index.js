8e18cbf7012cb44ac2569bd9c4f6eb1d549858d4
Julian Rosenblum <jsrmath@gmail.com>
2015-07-14 21:13:37 -0400

    Implemented splats and refactored

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var _ = require('underscore');
+var assert = require('assert');
 
 var ArgumentError = function(message) {
   var err = new Error();
@@ -7,11 +8,17 @@ var ArgumentError = function(message) {
   return err;
 };
 
-var ifUndef = function (val, def) {
-  return typeof val === 'undefined' ? def : val;
+// Standard, required argument
+var req = function (name) {
+  return {
+    req: true,
+    name: name
+  };
 };
 
-var opt = function (name, val) { // Optional argument, potentially with a default, e.g. wiz.opt('a', 1)
+// Optional argument, potentially with a default
+// e.g. wiz.opt('a', 1), wiz.opt('a')
+var opt = function (name, val) {
   return {
     opt: true,
     name: name,
@@ -19,7 +26,9 @@ var opt = function (name, val) { // Optional argument, potentially with a defaul
   };
 };
 
-var parse = function (name, func) { // Argument that should be parsed, e.g. wiz.parse('a', parseInt)
+// Argument that should be first passed into a given function
+// e.g. wiz.parse('a', parseInt)
+var parse = function (name, func) {
   return {
     parse: true,
     name: name,
@@ -27,49 +36,120 @@ var parse = function (name, func) { // Argument that should be parsed, e.g. wiz.
   };
 };
 
-var must = function (name, func) { // Argument that when passed to a given function must return true
+// Argument that when passed to a given function must return true
+// e.g. wiz.must('a', isFinite)
+var must = function (name, func) {
   return {
     must: true,
     name: name,
     func: func
   }
-}
+};
+
+// Argument that absorbs rest of unbound arguments in an array
+var splat = function (name) {
+  return {
+    splat: true,
+    name: name
+  }
+};
+
+var ifUndef = function (val, def) {
+  return typeof val === 'undefined' ? def : val;
+};
+
+var nonNegative = function (x) { // Return 0 if x is negative
+  return x < 0 ? 0 : x;
+};
+
+var isRequired = function (arg) {
+  return arg.req || arg.parse || arg.must;
+};
+
+var isOptional = function (arg) {
+  return arg.opt;
+};
+
+var isSplat = function (arg) {
+  return arg.splat;
+};
 
 // Take arguments and actual values and return object
 var makeArgs = function (args, vals) {
   var argObj = {};
+  var numVals = vals.length; // How many values are passed in
+  var numReq = _.filter(args, isRequired).length; // How many arguments are required
+  var numOpt = _.filter(args, isOptional).length; // How many requirements are optional (no splats)
+  var optAllowances = nonNegative(numVals - numReq); // How many values left over for optional arguments
+  var splatAllowances = nonNegative(optAllowances - numOpt); // How many values are left over for a splat
+  var i = 0;
 
-  _.each(args, function (arg, i) {
-    if (typeof(arg) === 'string') { // Regular argument, e.g. 'a'
-      argObj[arg] = vals[i];
-    }
-    else if (arg.opt) {
-      argObj[arg.name] = ifUndef(vals[i], arg.val);
-    }
-    else if (arg.parse) {
-      argObj[arg.name] = arg.func(vals[i]);
-    }
-    else if (arg.must) {
-      if (!arg.func(vals[i])) {
-        throw new ArgumentError('Argument "' + arg.name + '" failed `must` condition.');
+  _.each(args, function (arg) {
+    if (isRequired(arg)) {
+      if (i >= numVals) {
+        throw new ArgumentError('Missing value for required argument: ' + arg.name);
       }
-      argObj[arg.name] = vals[i];
+
+      else if (arg.req) {
+        argObj[arg.name] = vals[i];
+      }
+
+      else if (arg.parse) {
+        argObj[arg.name] = arg.func(vals[i]);
+      }
+
+      else if (arg.must) {
+        if (!arg.func(vals[i])) {
+          throw new ArgumentError('Argument "' + arg.name + '" failed `must` condition.');
+        }
+        argObj[arg.name] = vals[i];
+      }
+
+      i += 1;
     }
+
+    else if (isOptional(arg)) {      
+      if (optAllowances) {
+        assert(i < numVals);
+
+        argObj[arg.name] = vals[i];
+        optAllowances -= 1;
+        i += 1;
+      }
+
+      else { // Use default value
+        argObj[arg.name] = arg.val;
+      }
+    }
+
+    else if (isSplat(arg)) {
+      argObj[arg.name] = vals.slice(i, i + splatAllowances);
+      i += splatAllowances;
+      splatAllowances = 0; // We've used up all our splat allowances
+    }
+
+    else throw new ArgumentError('Invalid argument: ' + arg);
   });
 
   return argObj;
 };
 
+module.exports.req = req;
 module.exports.opt = opt;
 module.exports.parse = parse;
 module.exports.must = must;
+module.exports.splat = splat;
 
 module.exports.func = function (/* arguments */) {
   var func = _.last(arguments);
   var args = _.initial(arguments);
 
+  args = args.map(function (arg) { // Convert strings into req()'s
+    return typeof arg === 'string' ? req(arg) : arg;
+  });
+
   return function () { // Return function that takes arbitrary number of arguments
-    var argObj = makeArgs(args, arguments)
+    var argObj = makeArgs(args, _.toArray(arguments))
     return func(argObj); // Call function with argument object
   };
 };
--- test.js
+++ test.js
@@ -13,12 +13,10 @@ describe('#func', function () {
     assert.equal(args.c, 2);
   });
 
-  it('should create a function with optional arguments', function () {
-    var foo = wiz.func(wiz.opt('a', 5), function (args) {
-      return args;
+  it('should throw an error when a required argument is not passed a value', function () {
+    assert.throws(function () {
+      wiz.func('a', function () {})();
     });
-    assert.equal(foo().a, 5);
-    assert.equal(foo(3).a, 3);
   });
 
   it('should create a function with parsed arguments', function () {
@@ -33,5 +31,75 @@ describe('#func', function () {
     assert.throws(function () {
       foo(5);
     });
+  });
+
+  it('should create a function with optional arguments', function () {
+    var foo = wiz.func('a', wiz.opt('b'), 'c', wiz.opt('d', 3), function (args) {
+      return args;
+    });
+
+    var args = foo(0, 1, 2);
+    assert.equal(args.a, 0);
+    assert.equal(args.b, 1);
+    assert.equal(args.c, 2);
+    assert.equal(args.d, 3);
+
+    args = foo(0, 1);
+    assert.equal(args.a, 0);
+    assert.equal(args.b, null);
+    assert.equal(args.c, 1);
+    assert.equal(args.d, 3);
+  });
+
+  it('should create a function with a splat', function () {
+    var foo = wiz.func('a', wiz.splat('b'), 'c', function (args) {
+      return args;
+    });
+
+    var args = foo(0, 1, 2, 3, 4);
+    assert.equal(args.a, 0);
+    assert.equal(args.c, 4);
+    assert.equal(args.b.length, 3);
+
+    args = foo(0, 1);
+    assert.equal(args.a, 0);
+    assert.equal(args.c, 1);
+    assert(args.b);
+    assert.equal(args.b.length, 0);
+  });
+
+  it('should create a function with a splat and optional arguments', function () {
+    var foo = wiz.func(wiz.opt('a', 0), wiz.splat('b'), wiz.opt('c', 1), function (args) {
+      return args;
+    });
+
+    var args = foo(0, 1, 2, 3, 4);
+    assert.equal(args.a, 0);
+    assert.equal(args.c, 4);
+    assert.equal(args.b.length, 3);
+
+    args = foo(0, 1);
+    assert.equal(args.a, 0);
+    assert.equal(args.c, 1);
+    assert(args.b);
+    assert.equal(args.b.length, 0);
+
+    args = foo();
+    assert.equal(args.a, 0);
+    assert.equal(args.c, 1);
+  });
+
+  it('should create a function with two splats and leave the second empty', function () {
+    var foo = wiz.func('a', wiz.splat('b'), 'c', wiz.splat('d'), function (args) {
+      return args;
+    });
+
+    var args = foo(0, 1, 2, 3, 4);
+    assert.equal(args.a, 0);
+    assert.equal(args.c, 4);
+    assert(args.b);
+    assert(args.d);
+    assert.equal(args.b.length, 3);
+    assert.equal(args.d.length, 0);
   });
 });
